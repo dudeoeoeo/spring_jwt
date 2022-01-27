c3bfde3e17de6373c19400b5d54698f292cde606
dudeoeoeo <dudekekek@naver.com>
2022-01-28 06:52:32 +0900

    PrincipalDetails

---
+++ jwt-server/src/main/java/com/cos/jwt/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,24 @@
+package com.cos.jwt.config.auth;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+// http://localhost:8080/login 요청이 올 때 이 클래스가 동작한다
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("PrincipalDetailsService loadUserByUsername: " + username);
+        User user = userRepository.findByUsername(username);
+        return new PrincipalDetails(user);
+    }
+}
