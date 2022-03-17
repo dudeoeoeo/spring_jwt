8ad5d5fd4f2a4a6d0631e80f356c5976f6f90073
dudeoeoeo <dudekekek@naver.com>
2022-03-17 20:46:44 +0900

    JWT 테스트

--- jwt-server/src/test/java/com/cos/jwt/JWTControllerTest.java
+++ jwt-server/src/test/java/com/cos/jwt/JWTControllerTest.java
@@ -2,6 +2,7 @@ package com.cos.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.cos.jwt.config.jwt.JwtProperties;
 import org.junit.jupiter.api.DisplayName;
@@ -10,23 +11,75 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootTest
 public class JWTControllerTest {
 
+    static void printClaim(String key, Claim value) {
+        if(value.isNull()) {
+            System.out.printf("%s: {str}%s\n", key, value.asString());
+            return;
+        }
+        if(value.asString() != null) {
+            System.out.printf("%s:{str}%s\n", key, value.asString());
+            return;
+        }
+        if(value.asLong() != null) {
+            System.out.printf("%s:{lng}%d\n", key, value.asLong());
+            return;
+        }
+        if(value.asInt() != null) {
+            System.out.printf("%s:{int}%d\n", key, value.asInt());
+            return;
+        }
+        if(value.asBoolean() != null) {
+            System.out.printf("%s:{bol}%b\n", key, value.asBoolean());
+            return;
+        }
+        if(value.asDate() != null) {
+            System.out.printf("%s:{dte}%d\n", key, value.asDate().toString());
+            return;
+        }
+        if(value.asDouble() != null) {
+            System.out.printf("%s:{dbl}%f\n", key, value.asDouble());
+            return;
+        }
+        String [] values = value.asArray(String.class);
+        if (values != null) {
+            System.out.printf("%s:{arr}%s\n", key, Stream.of(values).collect(Collectors.toList()));
+            return;
+        }
+        Map valueMap = value.asMap();
+        if (valueMap != null) {
+            System.out.printf("%s:{map}%s\n", key, valueMap);
+            return;
+        }
+
+        System.out.println("======>> unknown type for : " + key);
+    }
+
+
     @DisplayName("Jwt 토큰 발급")
     @Test
     void JWT_토큰을_발급한다() {
+
+        Algorithm algorithm = Algorithm.HMAC512("test");
+
         String accessToken = JWT.create()
                 .withSubject("kay_token")
                 .withClaim("exp", Instant.now().getEpochSecond() + 3)
                 .withClaim("username", "user1")
-                .sign(Algorithm.HMAC512("test"));
+                .sign(algorithm);
 
         System.out.println("토큰: " + accessToken);
 
-        DecodedJWT decoded = JWT.decode(accessToken);
+        DecodedJWT decoded = JWT.require(algorithm).build().verify(accessToken);
 
-        System.out.println("decode: " + decoded);
+        printClaim("typ", decoded.getHeaderClaim("typ"));
+        printClaim("alg", decoded.getHeaderClaim("alg"));
+        decoded.getClaims().forEach(JWTControllerTest::printClaim);
     }
 }
