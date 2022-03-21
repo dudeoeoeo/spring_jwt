ef065d03d24817ebf6bbb14e87eb53ed288bdbc6
dudeoeoeo <dudekekek@naver.com>
2022-03-21 19:08:54 +0900

    admin test를 위한 컨트롤러 생성

---
+++ jwt-server/src/main/java/com/cos/jwt/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.cos.jwt.controller;
+
+import com.cos.jwt.model.User;
+import com.cos.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/userList")
+    public List<User> getUserList() {
+        return userRepository.findAll();
+    }
+}
