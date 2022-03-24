cb41ca02d8d78421778233f4e97c341091e4600a
dudeoeoeo <dudekekek@naver.com>
2022-03-24 20:49:56 +0900

    Querydsl 테스트를 위한 entity 추가

---
+++ jwt-server/src/main/java/com/cos/jwt/model/Option.java
@@ -0,0 +1,35 @@
+package com.cos.jwt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "option")
+public class Option {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "option_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "size")
+    private String size;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "extra_price")
+    private int extraPrice;
+
+    @Column(name = "stock")
+    private int stock;
+}
---
+++ jwt-server/src/main/java/com/cos/jwt/model/Product.java
@@ -0,0 +1,32 @@
+package com.cos.jwt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@ToString
+@Table(name = "product")
+public class Product {
+
+    @Id @Column(name = "product_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private int price;
+
+    @OneToMany(mappedBy = "product")
+    private List<Option> options = new ArrayList<>();
+}
