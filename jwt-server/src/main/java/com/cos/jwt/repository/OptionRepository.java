070db8da77f465382cb9b55eb49aabfe78436780
dudeoeoeo <dudekekek@naver.com>
2022-04-04 07:22:42 +0900

    상품 정보로 상품 옵션 가져오기

--- jwt-server/src/main/java/com/cos/jwt/repository/OptionRepository.java
+++ jwt-server/src/main/java/com/cos/jwt/repository/OptionRepository.java
@@ -1,7 +1,12 @@
 package com.cos.jwt.repository;
 
 import com.cos.jwt.model.Option;
+import com.cos.jwt.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
+
+    List<Option> findAllByProduct(Product product);
 }
