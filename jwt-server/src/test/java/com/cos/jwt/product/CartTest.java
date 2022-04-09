af2e0fd02fc7062a69a3b939ddfd2dc5e1099887
dudeoeoeo <dudekekek@naver.com>
2022-04-09 13:38:45 +0900

    상품 추가 테스트

--- jwt-server/src/test/java/com/cos/jwt/product/CartTest.java
+++ jwt-server/src/test/java/com/cos/jwt/product/CartTest.java
@@ -1,5 +1,6 @@
 package com.cos.jwt.product;
 
+import com.cos.jwt.model.Cart;
 import com.cos.jwt.model.Option;
 import com.cos.jwt.model.Product;
 import com.cos.jwt.repository.CartRepository;
@@ -7,6 +8,7 @@ import com.cos.jwt.repository.OptionRepository;
 import com.cos.jwt.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +17,12 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -43,6 +50,14 @@ public class CartTest {
 
     private ProductHelper productHelper = new ProductHelper();
 
+    @BeforeEach
+    void 기본_상품_옵션_생성() {
+        List<Product> productList = productHelper.상품생성();
+        productRepository.saveAll(productList);
+
+        List<Option> optionList = productHelper.상품옵션생성();
+        optionRepository.saveAll(optionList);
+    }
     @Test
     void 상품헬퍼테스트() {
         List<Product> productList = productHelper.상품생성();
@@ -58,5 +73,18 @@ public class CartTest {
         List<Option> options = optionRepository.findAll();
 
         options.forEach(System.out::println);
+    }
+
+    @Test
+    void 상품_장바구니_추가_테스트() {
+        Cart cart = new Cart();
+
+        List<Product> products = productRepository.findAll();
+        cart.setProductList(new ArrayList<>(Arrays.asList(products.get(0), products.get(1), products.get(2))));
+
+        Cart savedCart = cartRepository.save(cart);
+
+        System.out.println(savedCart.toString());
+        assertEquals(3, savedCart.getProductList().size());
     }
 }
